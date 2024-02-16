d8125f4507044d448a6969bb285d0ae7b52ecf75
D3mk4 <dmtvchenko@gmail.com>
2024-02-16 18:34:46 +0200

    current page

--- src/components/Header/UserNav/UserNav.jsx
+++ src/components/Header/UserNav/UserNav.jsx
@@ -1,8 +1,14 @@
-import { NavLink } from 'react-router-dom';
+import { NavLink, useLocation } from 'react-router-dom';
 import { NavContainer } from './UserNav.styled';
 
 const UserNav = () => {
   const isLogged = true;
+  const location = useLocation();
+  const currentLocation = location.pathname;
+
+  const homePage = currentLocation === '/';
+  const psyPage = currentLocation === '/psychologists';
+  const favoritePage = currentLocation === '/favorites';
 
   return (
     <NavContainer>
@@ -10,17 +16,20 @@ const UserNav = () => {
         <NavLink aria-label="To Home Page" to="/">
           Home
         </NavLink>
+        {homePage && <div></div>}
       </li>
       <li>
         <NavLink aria-label="To Psychologists List" to="/psychologists">
           Psychologists
         </NavLink>
+        {psyPage && <div></div>}
       </li>
       {isLogged && (
         <li>
           <NavLink aria-label="To Favorite List" to="/favorites">
             Favorites
           </NavLink>
+          {favoritePage && <div></div>}
         </li>
       )}
     </NavContainer>
--- src/components/Header/UserNav/UserNav.styled.js
+++ src/components/Header/UserNav/UserNav.styled.js
@@ -3,4 +3,19 @@ import styled from 'styled-components';
 export const NavContainer = styled.ul`
   display: flex;
   gap: 40px;
+
+  & > li {
+    position: relative;
+
+    & > div {
+      position: absolute;
+      top: 27px;
+      left: 50%;
+      transform: translate(-50%, -50%);
+      width: 8px;
+      height: 8px;
+      border-radius: 100%;
+      background: var(--green);
+    }
+  }
 `;
