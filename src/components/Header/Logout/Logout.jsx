703adb3d978c79cda20f867a655b9944c6782e5f
D3mk4 <dmtvchenko@gmail.com>
2024-03-19 14:51:23 +0200

    add default

--- src/components/Header/Logout/Logout.jsx
+++ src/components/Header/Logout/Logout.jsx
@@ -4,11 +4,16 @@ import {
   LogoutContainer,
   UserNameWrapper,
 } from './Logout.styled';
+import usual from '../../../styles/image/defaultImg/default@1x.webp';
+import retina from '../../../styles/image/defaultImg/default@2x.webp';
 import { useDispatch, useSelector } from 'react-redux';
 import { selectUser } from '../../../redux/auth/selectors';
 import { logOut } from '../../../redux/auth/operations';
+import { useState } from 'react';
 
 const Logout = () => {
+  const [imageLoadError, setImageLoadError] = useState(false);
+
   const user = useSelector(selectUser);
   const dispatch = useDispatch();
   const { name, avatarURL } = user;
@@ -17,10 +22,20 @@ const Logout = () => {
     dispatch(logOut());
   };
 
+  const handleImageError = () => setImageLoadError(true);
+
   return (
     <LogoutContainer>
       <li>
-        <AvatarContainer src={avatarURL} alt={name} />
+        {imageLoadError ? (
+          <img srcSet={`${usual} 1x, ${retina} 2x`} alt={name} />
+        ) : (
+          <AvatarContainer
+            src={avatarURL}
+            alt={name}
+            onError={handleImageError}
+          />
+        )}
         <UserNameWrapper>{name}</UserNameWrapper>
       </li>
       <li>
